11e8a2a93ad08eee26318a2464792b03014bb2f4
Michal-Naron <michanaron@gmail.com>
2025-04-21 13:28:21 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,15 +2,15 @@ from typing import Callable, Any
 
 
 def cache(func: Callable) -> Callable:
-    cache_list = []  # prototype [[<args>, value]]
+    cache_list = []
 
     def inner(*args, **kwargs) -> Any:
-        for i in cache_list:
-            if i[0] in args or i[0] in kwargs:
+        for cached_args, cached_kwargs, result in cache_list:
+            if cached_args == args and cached_kwargs == kwargs:
                 print("Getting from cache")
-                return i[1]
+                return result
         function_result = func(*args, **kwargs)
-        cache_list.append([*args, function_result])  # missing kwargs
+        cache_list.append([args, kwargs, function_result])
         print("Calculating new result")
         return function_result
 
